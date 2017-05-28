7bbd5a6683328b383d682f732df36f0a00f990f2
ig0ne <igone000@gmail.com>
2017-05-28 04:49:19 +0200

    Update cokejob.sql

--- esx_cokejob/cokejob.sql
+++ esx_cokejob/cokejob.sql
@@ -2,6 +2,6 @@ INSERT INTO `items` (`name`, `label`)
 VALUES
 	('feuille_coca', 'Feuille de coca'),
 	('jus_coca', 'Jus de coca'),
-  ('pate_coca', 'Pâte de coca'),
+  	('pate_coca', 'Pâte de coca'),
 	('coca', 'Cocaïne')
 ;
